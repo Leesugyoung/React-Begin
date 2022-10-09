6d284c558704681945bb251422cc849319c02316
sugyoung lee <tnrud4685@naver.com>
2022-10-09 18:31:30 +0900

    basic

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -1,13 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+    useEffect(() => {
+      console.log("hi :)");
+      return () => console.log("bye :(")
+    }, []);
+    return <h1>Hello</h1>;
+}
 
 function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!!!!!!</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return <div>
+    {showing ? <Hello /> : null}
+    <button onClick={onClick}>{showing ? "Hide " : "Show"}</button>
+  </div>
 }
 
 export default App;
